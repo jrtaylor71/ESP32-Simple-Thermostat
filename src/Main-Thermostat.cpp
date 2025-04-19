19ab0377980d8cef6d499c14214f68805592db17
Jonn Taylor <jrtaylor@taylordatacom.com>
2025-04-18 20:18:17 -0500

    modified:   .pio/build/esp32dev/.sconsign310.dblite modified:   .pio/build/esp32dev/firmware.bin modified:   .pio/build/esp32dev/firmware.elf modified:   .pio/build/esp32dev/firmware.map modified:   .pio/build/esp32dev/src/Main-Thermostat.cpp.o modified:   src/Main-Thermostat.cpp

--- src/Main-Thermostat.cpp
+++ src/Main-Thermostat.cpp
@@ -1,3 +1,22 @@
+/*
+ * This file is part of ESP32-Simple-Thermostat.
+ *
+ * ESP32-Simple-Thermostat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ESP32-Simple-Thermostat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/* Copyright (c) 2025 Jonn Taylor (jrtaylor@taylordatacom.com) */
+
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include <Preferences.h>
